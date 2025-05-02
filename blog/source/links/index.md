9b357c01a69d55d512a87e6c786ad9ee82b063d4
runoneall <runoobsteve@hotmail.com>
2025-05-02 12:28:14 +0800

    update

---
+++ blog/source/links/index.md
@@ -0,0 +1,6 @@
+---
+title: Links
+date: 2025-05-01 11:02:00
+---
+
+This Is Links Page
--- blog/themes/hexo-theme-old-fashion/source/js/search.js
+++ blog/themes/hexo-theme-old-fashion/source/js/search.js
@@ -8,50 +8,50 @@ articleSearchNotFound.style.display = 'none';
 mainFrame.appendChild(articleSearchNotFound);
 
 function SearchInit() {
-  mainFrame.querySelectorAll('article').forEach(article => {
-    article.style.display = 'block';
-  })
-  articleSearchNotFound.style.display = 'none';
+    mainFrame.querySelectorAll('article').forEach(article => {
+        article.style.display = 'block';
+    })
+    articleSearchNotFound.style.display = 'none';
 
-  const targetSearch = location.hash.slice(3)
-  if (targetSearch !== '') {
-    SearchArticle();
-    return;
-  }
-  searchInput.value = '';
-  location.hash = '';
+    const targetSearch = location.hash.slice(3)
+    if (targetSearch !== '') {
+        SearchArticle();
+        return;
+    }
+    searchInput.value = '';
+    location.hash = '';
 }
 
 SearchInit();
 
 searchFrom.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const searchValue = decodeURIComponent(searchInput.value).trim();
-  SearchInit();
-  if (!searchValue) {
-    return;
-  }
-  if (location.pathname !== '/index.html') {
-    location.href = '/index.html#s=' + encodeURIComponent(searchValue);
-  } else {
-    location.hash = '#s=' + encodeURIComponent(searchValue);
-  }
+    event.preventDefault();
+    const searchValue = decodeURIComponent(searchInput.value).trim();
+    SearchInit();
+    if (!searchValue) {
+        return;
+    }
+    if (location.pathname !== '/index.html') {
+        location.href = '/index.html#s=' + encodeURIComponent(searchValue);
+    } else {
+        location.hash = '#s=' + encodeURIComponent(searchValue);
+    }
 })
 
 function SearchArticle() {
-  const searchValue = decodeURIComponent(location.hash.slice(3));
-  var is_not_found = true;
-  const articleList = mainFrame.querySelectorAll('article');
-  articleList.forEach(article => {
-    if (!article.textContent.toLowerCase().includes(searchValue.toLowerCase())) {
-      article.style.display = 'none';
-    } else {
-      is_not_found = false;
+    const searchValue = decodeURIComponent(location.hash.slice(3));
+    var is_not_found = true;
+    const articleList = mainFrame.querySelectorAll('article');
+    articleList.forEach(article => {
+        if (!article.textContent.toLowerCase().includes(searchValue.toLowerCase())) {
+            article.style.display = 'none';
+        } else {
+            is_not_found = false;
+        }
+    });
+    if (is_not_found) {
+        articleSearchNotFound.style.display = 'block';
     }
-  });
-  if (is_not_found) {
-    articleSearchNotFound.style.display = 'block';
-  }
 }
 
 window.addEventListener('hashchange', () => SearchArticle());
